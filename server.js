81d17a07ba0b7b16b8966cb52618b5c63e02ad2b
Antonia Garci <antonia@Antonias-MacBook-Air.local>
2021-12-15 21:09:10 -0500

    finished server.js and user-routes.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
-const session = require('session');
-const handlebars = require('handlebars');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const SequelizeStore=require('connect-session-sequelize')(session.Store);
 
 const routes=require('./controllers');
