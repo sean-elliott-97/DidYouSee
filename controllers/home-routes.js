5a0fd78057ec3e93153959b39bda8743ca6cc1e4
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-12-19 16:53:56 -0500

    updated handlebars view, added route, modified login.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,26 +6,44 @@ const withAuth = require("../utils/auth");
 router.get("/", async (req, res) => {
   res.render("homepage", {
     loggedIn: req.session.loggedIn,
+
   });
+  
 });
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/movieData");
+    res.redirect("/movie");
+   
     return;
   }
 
   res.render("login");
 });
 
-// router.get('/movieData',(req,res)=>{
-// const movies =  Movie.findAll();
-// res.render('movie',{movies});
-// })
+//getting all movies
+router.get("/movie",withAuth,async(req,res)=>{
+  
+  try {
+    const allMovies = await Movie.findAll();
+    //console.log(allMovies);
+    for(let i =0;i<allMovies.length;i++){
+      console.log(allMovies[i].dataValues.title);
+    }
+    
+    //const movies = dbMovieData.get({ plain: true });
+    //results = results.map( (r) => ( r.toJSON() ) )
+    res.render("allMovies", { allMovies, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  });
+
+
+//getting movie by id  
 router.get("/movie/:id", withAuth, async (req, res) => {
   try {
-    const dbMovieData = await Movie.findByPk(req.params.id, 
-     
-    );
+    const dbMovieData = await Movie.findByPk(req.params.id,);
     console.log(dbMovieData);
     const movies = dbMovieData.get({ plain: true });
 
