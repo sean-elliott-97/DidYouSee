c66fc5b77881c1190d9669898969ab735445b00f
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-12-21 00:11:29 -0500

    updated createList.js

--- public/js/createList.js
+++ public/js/createList.js
@@ -1,18 +1,51 @@
-$("#1").click(function(){
-    alert("1");
-})
-$("#2").click(function(){
-    alert("2");
-})
-$("#3").click(function(){
-    alert("3");
-})
-$("#4").click(function(){
-    alert("4");
-})
-$("#5").click(function(){
-    alert("5");
-})
-$("#6").click(function(){
-    alert("6");
-})
+var movieCardArray = [
+  {
+    id: "",
+    title: "",
+  },
+];
+
+$("#1").click(function () {
+  let movieCardTitle = $(this).siblings("h5").text();
+  movieCardArray.push({ 1: movieCardTitle });
+
+  //alert(JSON.stringify(movieCardArray));
+
+  $("#1").hide();
+});
+$("#2").click(function () {
+  let movieCardTitle = $(this).siblings("h5").text();
+  movieCardArray.push({ 2: movieCardTitle });
+
+ // alert(JSON.stringify(movieCardArray));
+  $("#2").hide();
+});
+$("#3").click(function () {
+  let movieCardTitle = $(this).siblings("h5").text();
+  movieCardArray.push({ 3: movieCardTitle });
+
+  //alert(JSON.stringify(movieCardArray));
+  $("#3").hide();
+});
+$("#4").click(function () {
+  let movieCardTitle = $(this).siblings("h5").text();
+  movieCardArray.push({ 4: movieCardTitle });
+
+ // alert(JSON.stringify(movieCardArray));
+  $("#4").hide();
+});
+$("#5").click(function () {
+  let movieCardTitle = $(this).siblings("h5").text();
+  movieCardArray.push({ 5: movieCardTitle });
+
+  //alert(JSON.stringify(movieCardArray));
+  $("#5").hide();
+});
+$("#6").click(function () {
+  let movieCardTitle = $(this).siblings("h5").text();
+  movieCardArray.push({ 6: movieCardTitle });
+
+  //alert(JSON.stringify(movieCardArray));
+  $("#6").hide();
+
+});
