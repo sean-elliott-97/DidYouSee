57ee6a375ab6a343fae3a2f8e0526ec7bbe289ce
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-12-20 19:52:33 -0500

    added JawsDb

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,49 +1,53 @@
 const router = require("express").Router();
-const { Movie, Gallery } = require("../models");
+const { Movie, List } = require("../models");
 // Import the custom middleware
 const withAuth = require("../utils/auth");
 
+//get route for homepage
 router.get("/", async (req, res) => {
   res.render("homepage", {
     loggedIn: req.session.loggedIn,
-
   });
-  
 });
+
+//get route for login page
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/movie");
-   
+
     return;
   }
 
   res.render("login");
 });
 
+//get route for getting all lists
+router.get("/list", async (req, res) => {
+  try {
+    const allLists = await List.findAll();
+    res.render("list", { allLists, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 //getting all movies
-router.get("/movie",withAuth,async(req,res)=>{
-  
+router.get("/movie", withAuth, async (req, res) => {
   try {
     const allMovies = await Movie.findAll();
-    //console.log(allMovies);
-    for(let i =0;i<allMovies.length;i++){
-      console.log(allMovies[i].dataValues.title);
-    }
-    
-    //const movies = dbMovieData.get({ plain: true });
-    //results = results.map( (r) => ( r.toJSON() ) )
+  
     res.render("allMovies", { allMovies, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
-  });
+});
 
-
-//getting movie by id  
+//getting movie by id
 router.get("/movie/:id", withAuth, async (req, res) => {
   try {
-    const dbMovieData = await Movie.findByPk(req.params.id,);
+    const dbMovieData = await Movie.findByPk(req.params.id);
     console.log(dbMovieData);
     const movies = dbMovieData.get({ plain: true });
 
