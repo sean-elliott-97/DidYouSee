b703a1ab4665880cf295f16dd38ac8bb0ccad3d8
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-12-20 21:32:35 -0500

    updated views

---
+++ public/js/createList.js
@@ -0,0 +1,18 @@
+$("#1").click(function(){
+    alert("1");
+})
+$("#2").click(function(){
+    alert("2");
+})
+$("#3").click(function(){
+    alert("3");
+})
+$("#4").click(function(){
+    alert("4");
+})
+$("#5").click(function(){
+    alert("5");
+})
+$("#6").click(function(){
+    alert("6");
+})
