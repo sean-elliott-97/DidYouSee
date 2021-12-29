1bea06bc288eeea784ede0a34d4257304321292f
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-12-29 17:42:35 -0500

    added axios, updated handlebars, changed routes in home-routes, put api key in .env file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,12 +3,12 @@ const { Movie, List, User } = require("../models");
 // Import the custom middleware
 const withAuth = require("../utils/auth");
 const bodyParser = require("body-parser");
-
+const axios = require("axios");
+const env = require("dotenv").config();
 router.use(bodyParser.urlencoded());
-
+const API_KEY = process.env.MOVIE_DB_KEY;
 //get route for homepage
 router.get("/", async (req, res) => {
-  
   res.render("homepage", {
     loggedIn: req.session.loggedIn,
   });
@@ -17,8 +17,7 @@ router.get("/", async (req, res) => {
 //get route for login page
 router.get("/login", async (req, res) => {
   if (req.session.loggedIn) {
-    
-    res.redirect("/movie");
+    res.redirect("/list");
 
     return;
   }
@@ -26,47 +25,59 @@ router.get("/login", async (req, res) => {
   res.render("login");
 });
 
-//get movie search
-router.get("/movie", async (req, res) => {
-  console.log(req.session.user_id);
-  res.render("movieSearch", { loggedIn: req.session.loggedIn });
+//get movie
+// router.get("/movie", async (req, res) => {
+//   // console.log(req.session.user_id);
+
+//   res.render("movieSearch", { loggedIn: req.session.loggedIn });
+// });
+
+//add movie to list
+router.post("/list", async (req, res) => {
+  let movieTitle = req.body[0];
+
+  axios
+    .get(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieTitle}`)
+    .then((response) => {
+      let movieData = response.data;
+       console.log(movieData.Response);
+       if(movieData.Response=="False"){
+         res.redirect('/list');
+         return;
+       }
+       if(movieData.Response=="True"){
+        Movie.create({
+          title: movieData.Title,
+          plot: movieData.Plot,
+          poster: movieData.Poster,
+          list_id: req.session.user_id,
+        }).catch((err) => {
+          console.log(err);
+        });
+        res.redirect('/list');
+        return;
+       }
+    
+    })
+    .catch((err) => console.log(err));
+  
 });
 
-//post movie
-router.post("/movie",async(req,res)=>{
-  try {
-    const dbMovieData = await Movie.create({
-      title: req.body[0],
-      plot: req.body[1],
-      poster:req.body[2],
-      list_id:req.session.user_id
-      
-
-    }
-    );
-  //  res.redirect('/movie'); 
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-  
-})
-
-router.get("/list",async(req,res)=>{
+router.get("/list", async (req, res) => {
   const loggedUserData = await Movie.findAll({
-    where:{
-      list_id:req.session.user_id
-    }
-  })
-  console.log(loggedUserData);
-  res.render("list",{loggedUserData,loggedIn:req.session.loggedIn});
-})
+    where: {
+      list_id: req.session.user_id,
+    },
+  });
+ // console.log(loggedUserData);
+  res.render("list", { loggedUserData, loggedIn: req.session.loggedIn });
+});
 //get route for movie item by id
 router.get("/movie/:id", withAuth, async (req, res) => {
   try {
     const dbMovieData = await Movie.findByPk(req.params.id);
     console.log(dbMovieData);
-   
+
     const movie = dbMovieData.get({ plain: true });
 
     res.render("movie", { movie, loggedIn: req.session.loggedIn });
@@ -89,5 +100,3 @@ router.delete("/movie/:id", withAuth, async (req, res) => {
 });
 
 module.exports = router;
-
-
--- public/js/addMovie.js
+++ public/js/addMovie.js
@@ -1,60 +1,22 @@
 //allows users to search for a movie by title
-
-//must refresh page before searching for and adding a movie
-
-var currentMovieData =[];
-
-
 $("#search").click(async function () {
-  
-  //fetches movie data from omdb
+  let movieTitle = [$("#movieName").val()];
 
-  //still have to hide api_key
-  fetch(`https://www.omdbapi.com/?apikey=99ada8e&t=${$("#movieName").val()}`)
-    .then((response) => response.json())
-    .then((data) => {
-      
-      //displays movie title
-      $("#mTitle").text(data.Title);
-      //displays add to list button
-      $("#mAddToList").css("visibility", "visible");
-
-      currentMovieData=[data.Title,data.Plot,data.Poster];
-      console.log(currentMovieData);
-     
-     
-
-    });
-    $("#mAddToList").click(async function () {
-      //hides movieSearch div    
-      $("#movieSearch").css("visibility","hidden");
-      //decide on other movie info to include
-      
-      //removes add to list button and movie title
-      $("#mAddToList").remove();
-      $("#mTitle").remove();
-      
-      $("#refresh").css("visibility","visible");  
-        const options = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(currentMovieData),
-        };
-        
-        const res = await fetch("/movie", options).then(( response) => {
-          
-          
-          
-          console.log(response.json());
-          
-        })
-        
-        
-      })
-  
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(movieTitle),
+  };
+  console.log(movieTitle);
+ 
+  const res = await fetch("/list", options).then((response) => {
+    console.log(response);
+    console.log(movieTitle);
+    if(movieTitle!==''){
+      window.location.reload();
+    }
+  });
+ 
 });
-$("#refresh").click(function(){
-  location.reload();
-})
