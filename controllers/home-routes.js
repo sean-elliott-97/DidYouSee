4f3d4a41c9e1b04fd18186a3e0247515ebb7d76d
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-12-30 15:41:04 -0500

    changed routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 //get route for login page
 router.get("/login", async (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/api/list/");
+    res.redirect("/list");
 
     return;
   }
@@ -55,4 +55,50 @@ router.delete("/movie/:id", withAuth, async (req, res) => {
   });
 });
 
+router.get("/list", async (req, res) => {
+  const loggedUserData = await Movie.findAll({
+    where: {
+      list_id: req.session.user_id,
+    },
+  });
+ // console.log(loggedUserData);
+  res.render("list", { loggedUserData, loggedIn: req.session.loggedIn });
+});
+router.post("/list", async (req, res) => {
+  let movieTitle = req.body[0];
+try{
+  axios
+    .get(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieTitle}`)
+    .then((response) => {
+      let movieData = response.data;
+      console.log(movieData);
+       console.log(movieData.Response);
+       if(movieData.Response=="False"){
+         res.redirect('/');
+         return;
+       }
+       if(movieData.Response=="True"){
+        Movie.create({
+          title: movieData.Title,
+          plot: movieData.Plot,
+          poster: movieData.Poster,
+          rating:movieData.Rated,
+          director:movieData.Director,
+          actors:movieData.Actors,
+          runtime:movieData.Runtime,
+          list_id: req.session.user_id,
+        }).catch((err) => {
+          console.log(err);
+        });
+        res.redirect('/');
+        return;
+       }
+    
+    })
+    .catch((err) => console.log(err));
+}catch(err){
+  console.log(err);
+  res.status(500).json(err);
+}
+});
 module.exports = router;
