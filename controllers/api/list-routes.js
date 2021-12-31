b5ac137c03e87e857a4ce6e9dc8d9f9c90dfa5a1
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-12-31 09:17:15 -0500

    changed routes

--- controllers/api/list-routes.js
+++ controllers/api/list-routes.js
@@ -5,52 +5,52 @@ const env = require("dotenv").config();
 const API_KEY = process.env.MOVIE_DB_KEY;
 // const withAuth = require("../utils/auth");
 //add movie to list
-// router.get("/", async (req, res) => {
-//     const loggedUserData = await Movie.findAll({
-//       where: {
-//         list_id: req.session.user_id,
-//       },
-//     });
-//    // console.log(loggedUserData);
-//     res.render("list", { loggedUserData, loggedIn: req.session.loggedIn });
-//   });
-// router.post("/", async (req, res) => {
-//     let movieTitle = req.body[0];
-//   try{
-//     axios
-//       .get(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieTitle}`)
-//       .then((response) => {
-//         let movieData = response.data;
-//         console.log(movieData);
-//          console.log(movieData.Response);
-//          if(movieData.Response=="False"){
-//            res.redirect('/');
-//            return;
-//          }
-//          if(movieData.Response=="True"){
-//           Movie.create({
-//             title: movieData.Title,
-//             plot: movieData.Plot,
-//             poster: movieData.Poster,
-//             rating:movieData.Rated,
-//             director:movieData.Director,
-//             actors:movieData.Actors,
-//             runtime:movieData.Runtime,
-//             list_id: req.session.user_id,
-//           }).catch((err) => {
-//             console.log(err);
-//           });
-//           res.redirect('/');
-//           return;
-//          }
+router.get("/", async (req, res) => {
+    const loggedUserData = await Movie.findAll({
+      where: {
+        list_id: req.session.user_id,
+      },
+    });
+   // console.log(loggedUserData);
+    res.render("list", { loggedUserData, loggedIn: req.session.loggedIn });
+  });
+router.post("/", async (req, res) => {
+    let movieTitle = req.body[0];
+  try{
+    axios
+      .get(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieTitle}`)
+      .then((response) => {
+        let movieData = response.data;
+        console.log(movieData);
+         console.log(movieData.Response);
+         if(movieData.Response=="False"){
+           res.redirect('/');
+           return;
+         }
+         if(movieData.Response=="True"){
+          Movie.create({
+            title: movieData.Title,
+            plot: movieData.Plot,
+            poster: movieData.Poster,
+            rating:movieData.Rated,
+            director:movieData.Director,
+            actors:movieData.Actors,
+            runtime:movieData.Runtime,
+            list_id: req.session.user_id,
+          }).catch((err) => {
+            console.log(err);
+          });
+          res.redirect('/');
+          return;
+         }
       
-//       })
-//       .catch((err) => console.log(err));
-//   }catch(err){
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-//   });
+      })
+      .catch((err) => console.log(err));
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err);
+  }
+  });
   
 
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -55,55 +55,55 @@ router.delete("/movie/:id", withAuth, async (req, res) => {
   });
 });
 
-router.get("/list", async (req, res) => {
-  try{
-  const loggedUserData = await Movie.findAll({
-    where: {
-      list_id: req.session.user_id,
-    },
-  });
- // console.log(loggedUserData);
-  res.render("list", { loggedUserData, loggedIn: req.session.loggedIn });
-  }catch(err){
-    console.log(err);
-  res.status(500).json(err);
-  }
-});
-router.post("/list", async (req, res) => {
-  let movieTitle = req.body[0];
-try{
-  axios
-    .get(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieTitle}`)
-    .then((response) => {
-      let movieData = response.data;
-      console.log(movieData);
-       console.log(movieData.Response);
-       if(movieData.Response=="False"){
-         res.redirect('/');
-         return;
-       }
-       if(movieData.Response=="True"){
-        Movie.create({
-          title: movieData.Title,
-          plot: movieData.Plot,
-          poster: movieData.Poster,
-          rating:movieData.Rated,
-          director:movieData.Director,
-          actors:movieData.Actors,
-          runtime:movieData.Runtime,
-          list_id: req.session.user_id,
-        }).catch((err) => {
-          console.log(err);
-        });
-        res.redirect('/');
-        return;
-       }
+// router.get("/list", async (req, res) => {
+//   try{
+//   const loggedUserData = await Movie.findAll({
+//     where: {
+//       list_id: req.session.user_id,
+//     },
+//   });
+//  // console.log(loggedUserData);
+//   res.render("list", { loggedUserData, loggedIn: req.session.loggedIn });
+//   }catch(err){
+//     console.log(err);
+//   res.status(500).json(err);
+//   }
+// });
+// router.post("/list", async (req, res) => {
+//   let movieTitle = req.body[0];
+// try{
+//   axios
+//     .get(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieTitle}`)
+//     .then((response) => {
+//       let movieData = response.data;
+//       console.log(movieData);
+//        console.log(movieData.Response);
+//        if(movieData.Response=="False"){
+//          res.redirect('/');
+//          return;
+//        }
+//        if(movieData.Response=="True"){
+//         Movie.create({
+//           title: movieData.Title,
+//           plot: movieData.Plot,
+//           poster: movieData.Poster,
+//           rating:movieData.Rated,
+//           director:movieData.Director,
+//           actors:movieData.Actors,
+//           runtime:movieData.Runtime,
+//           list_id: req.session.user_id,
+//         }).catch((err) => {
+//           console.log(err);
+//         });
+//         res.redirect('/');
+//         return;
+//        }
     
-    })
-    .catch((err) => console.log(err));
-}catch(err){
-  console.log(err);
-  res.status(500).json(err);
-}
-});
+//     })
+//     .catch((err) => console.log(err));
+// }catch(err){
+//   console.log(err);
+//   res.status(500).json(err);
+// }
+// });
 module.exports = router;
